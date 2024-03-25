e27fa98f31095fe6a42872ed5d3d246e22b7d12f
Aditya Gupta <113687793+Adityagupta9@users.noreply.github.com>
2024-03-25 21:53:22 +0530

    done

--- script2.js
+++ script2.js
@@ -2,8 +2,7 @@ document.getElementById('downloadButton').addEventListener('click', function () 
   // Define the URLs of the images you want to download
   var imageUrls = [
       "bdc4.jpg",
-      "bdc5.jpg",
-      "bdc6.jpg"
+      "bdc5.jpg"
   ];
 
   // Create an anchor element for each image
